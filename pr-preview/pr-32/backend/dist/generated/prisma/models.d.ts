64b9f02ae46331db2b48d50cf512afa4d7bd4d53
cedricrgt <cedricrgt@users.noreply.github.com>
2026-06-04 15:20:34 +0000

    Deploy preview for PR 32 🛫

--- pr-preview/pr-32/backend/dist/generated/prisma/models.d.ts
+++
@@ -1,8 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/Course.js';
-export type * from './models/Workshop.js';
-export type * from './models/Chapter.js';
-export type * from './models/Content.js';
-export type * from './models/Enrollment.js';
-export type * from './models/UserWorkshop.js';
-export type * from './commonInputTypes.js';
--- pr-preview/pr-32/backend/dist/src/user/dto/create-user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from '@prisma/client';
-export declare class CreateUserDto {
-    userName: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    role?: Role;
-}
